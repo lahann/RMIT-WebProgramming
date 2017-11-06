f9d49bd5d23f187b005c772ea1846062545e3c23
LauridsA <lauridsandersen2013@gmail.com>
2017-11-06 16:27:55 +0700

    works now

--- App.jsx
+++ App.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import Hello from './Hello.jsx'
-import Greeting from './Greeting.jsx'
-import Bye from './Bye.jsx'
-import StudentForm from './StudentForm.jsx'
-import Product from './Product.jsx'
+
 
 export default class App extends React.Component{
     render(){
