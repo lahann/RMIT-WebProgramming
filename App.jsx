892319a067931a7ece8c973eed013dabe526fa32
LauridsA <lauridsandersen2013@gmail.com>
2017-11-06 16:18:41 +0700

    npm installed

--- App.jsx
+++ App.jsx
@@ -5,20 +5,11 @@ import Bye from './Bye.jsx'
 import StudentForm from './StudentForm.jsx'
 import Product from './Product.jsx'
 
-const Afunction = bla => {
-    return(
-        <div>Returned a div {bla.name}</div>
-    )
-}
-
 export default class App extends React.Component{
     render(){
         return(
             <div>
-            <StudentForm />
-            <Afunction name="This is the name"/>
-
-
+            <div>Clearly an awesome start</div>
 
             </div>
             
--- Bye.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class Bye extends React.Component{
-    render(){
-        return(
-            <div> Goodbye {this.props.message}</div>
-        )
-    }
-}
