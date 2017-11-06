0450e72417e4042b9c8b991a709737ecf9480820
LauridsA <lauridsandersen2013@gmail.com>
2017-11-06 22:08:36 +0700

    how to do this again ..

--- App.jsx
+++ App.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
 
 
+
 export default class App extends React.Component{
     render(){
         return(
+            <html lang="en">
             <div>
             <div>Clearly an awesome start</div>
+            <div>
+            <table class="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
 
             </div>
-            
+            </div>
+            </html>
             )
     }
 }
