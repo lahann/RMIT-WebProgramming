a5577afe48231d08803657fe75a3c29fd2ee35c7
Tim Lahann <lahann@dakosy.de>
2017-11-08 17:24:40 +0700

    Test

--- App.jsx
+++ App.jsx
@@ -2,48 +2,55 @@ import React from 'react'
 
 
 
-export default class App extends React.Component{
-    render(){
-        return(
-            <html lang="en">
-            <div>
-            <div>other change</div>
-            <div>Clearly an awesome start</div>
-            <div>
+export default class App extends React.Component {
+  render() {
+    return (
+      <html lang="en">
+        <div>
+          <div>other change</div>
+          <div>Clearly an awesome start</div>
+          <div>
             <table class="table table-dark">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">First Name</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
 
-            </div>
-            </div>
-            </html>
-            )
-    }
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+      </html>
+    )
+  }
 }
