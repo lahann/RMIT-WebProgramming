afda0f813b582c5fd6eac4ab051dbf87671b8c98
LauridsA <lauridsandersen2013@gmail.com>
2017-11-10 16:12:15 +0700

    blah

--- header.jsx
+++ header.jsx
@@ -18,8 +18,8 @@ export default class Header extends React.Component{
       </Navbar.Brand>
     </Navbar.Header>
     <Nav>
-      <NavItem eventKey={1} href="#">Link</NavItem>
-      <NavItem eventKey={2} href="#">Link</NavItem>
+      <NavItem eventKey={1} href="#">Product Page</NavItem>
+      <NavItem eventKey={2} href="#">User??</NavItem>
       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
         <MenuItem eventKey={3.1}>Action</MenuItem>
         <MenuItem eventKey={3.2}>Another action</MenuItem>
