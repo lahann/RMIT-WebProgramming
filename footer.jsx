91bf0db17954d072caca14063015b31c1504c426
LauridsA <lauridsandersen2013@gmail.com>
2017-11-11 13:33:53 +0700

    footer looks like sth now. nice

--- footer.jsx
+++ footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Col, Row, Grid, xs, md, code, Panel, bsStyle} from 'react-bootstrap'
 export default class Footer extends React.Component {
 
     constructor(){
@@ -9,7 +9,30 @@ export default class Footer extends React.Component {
     render(){
         return(
             <div>
-                <h1>blabla</h1>
+                <Grid>
+                    <Row className="show-grid">
+                        <Col xs={4.5} md={3}><code>&lt;{ 'Questions?' }/&gt; <ul>
+                            <li> Help us help you</li>
+                            <li> not really, good luck</li>
+                            <li> we're still friends, right? cool</li>
+                            </ul> </code></Col>
+                        <Col xs={4.5} md={3}><code>&lt;{'Follow us'} /&gt;<ul>
+                            <li> Github or linkedin or sth</li>
+                            <li> Instagram of course</li>
+                            <li> we're cool</li>
+                            </ul></code></Col>
+                        <Col xs={4.5} md={3}><code>&lt;{'Our cool bizz!'} /&gt;<ul>
+                            <li> Get a job (slave job?)</li>
+                            <li> Seriously, we can't pay you</li>
+                            <li> you can still work for us, though</li>
+                            </ul></code></Col>
+                        <Col xs={4.5} md={3}><code>&lt;{'Sponsors and partners? Maybe?  '} /&gt;<ul>
+                            <li> Noone</li>
+                            <li> litterally noone</li>
+                            <li> We're not broke as long as we don't look at the bank account</li>
+                            </ul></code></Col>
+                    </Row>
+                </Grid>
             </div>
         )
     }
