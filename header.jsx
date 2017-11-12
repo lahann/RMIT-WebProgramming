52c4b0670ab1bf712f982e18ce1bfded30a701e9
LauridsA <lauridsandersen2013@gmail.com>
2017-11-12 13:21:29 +0700

    asdasd

--- header.jsx
+++ header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Navbar, NavDropdown, Nav, NavItem, MenuItem } from 'react-bootstrap'
+import { Button, Navbar, NavDropdown, Nav, NavItem, MenuItem, Grid } from 'react-bootstrap'
 export default class Header extends React.Component{
 
     constructor(){
@@ -12,12 +12,12 @@ export default class Header extends React.Component{
         return(
             <div> 
                <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
-      </Navbar.Brand>
-    </Navbar.Header>
-    <Nav>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <a href="#">React-Bootstrap</a>
+                </Navbar.Brand>
+              </Navbar.Header>
+              <Nav>
       <NavItem eventKey={1} href="#">Product Page</NavItem>
       <NavItem eventKey={2} href="#">User??</NavItem>
       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
@@ -27,6 +27,7 @@ export default class Header extends React.Component{
         <MenuItem divider />
         <MenuItem eventKey={3.4}>Separated link</MenuItem>
       </NavDropdown>
+      <MenuItem eventKey={4}> Shopping Cart</MenuItem>
     </Nav>
   </Navbar>
             </div>
