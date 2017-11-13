b0b5a4bfda9032a700f74c4975ff13f97f67dede
Tim Lahann <lahann@dakosy.de>
2017-11-13 11:42:39 +0700

    Implement Redux - Initial Commit

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { combineReducers, createStore } from 'redux'
 import App from './App.jsx'
 
+// action.type => String to identify our action
+// action.payload => Params for example
+
+// this.props.dispatch({type: 'ASD', payload: s})
+
+function product(state = [], action) {
+    return state;
+}
+
+function categorie(state = [], action) {
+    return state;
+}
+
+function shoppingCart(state = [], action) {
+    return state;
+}
+
+const centralState = combineReducers({
+    product,
+    categorie,
+    shoppingCart
+})
+
 ReactDOM.render(
-    <App/>, document.getElementById('app')
+    <Provider store={createStore(centralState)}>
+        <App />
+    </Provider>, document.getElementById('app')
 
 )
