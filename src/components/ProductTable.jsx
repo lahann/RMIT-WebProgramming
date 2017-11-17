1d1777b017664585bc69e198f77f4d8f1fa9ba6b
Linh Do <33480996+grass-jelly@users.noreply.github.com>
2017-11-17 15:53:17 +0700

    ProductTable component for admin-mode (static)

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Table, Button, ButtonToolbar } from 'react-bootstrap' 
+
+class ProductRow extends React.Component {
+    render() {
+       const product = this.props.product;
+       return (
+           <tr>
+               <td>{product.id}</td>
+               <td>{product.name}</td>
+               <td>{product.price}</td>
+               <td>{product.description}</td>
+               <td>{product.brand}</td>
+               <td>{product.producer}</td>
+               <td>{product.imageUrl}</td>
+               <td>
+               {
+                    <ButtonToolbar>
+                        <Button>EDIT</Button>
+                        <Button bsStyle="danger">DELETE</Button>
+                    </ButtonToolbar>
+
+               }
+               </td>
+           </tr>
+       );
+    }
+}
+export default class ProductTable extends React.Component {
+    render() {
+        const rows = []
+        this.props.products.forEach((product) => {
+            rows.push(
+                <ProductRow 
+                    product={product}
+                    key={product.name} />
+            )
+        })
+        return (
+            <Table striped bordered condensed hover> 
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        <th>Brand</th>
+                        <th>Producer</th>
+                        <th>Image URL</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </Table>
+        )
+        
+    }
+}
+
+// const PRODUCTS = [
+//     {id: 1, name: 'p1', price: '$29.99', description: 'a product', 
+//     brand: 'Originals', producer: 'adidas', imageUrl: '#'},
+//     {id: 2, name: 'p2', price: '$39.99', description: 'a product', 
+//     brand: 'NEO', producer: 'adidas', imageUrl: '#'},
+// ];
+
+// ReactDOM.render(
+//     <ProductTable products={PRODUCTS} />,
+//     document.getElementById('container')
+// );
