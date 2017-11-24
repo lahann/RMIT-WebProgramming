95a8e12d5565431565d1aafced6dcbf7960501d4
Linh Do <33480996+grass-jelly@users.noreply.github.com>
2017-11-24 19:46:43 +0700

    converted ProductTable to class (click to Edit inline)

--- src/admin-site/components/ProductTable.js
+++ src/admin-site/components/ProductTable.js
@@ -1,31 +1,64 @@
 import React from 'react'
 import ProductRow from './ProductRow'
+import EditProduct from './EditProduct.jsx'
 import { Table, Button, ButtonToolbar } from 'react-bootstrap' 
 
-const ProductTable = ({products}) => {
-    const rows = []
-    products.forEach(product => {
-        rows.push(
-            <ProductRow key={product.id} {...product}/>
-        )
-    })
-    return (
-        <Table striped bordered condensed hover> 
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Description</th>
-                    <th>Brand</th>
-                    <th>Producer</th>
-                    <th>Image URL</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-        </Table>
-    )
-}
+export default class ProductTable extends React.Component {
+    constructor() {
+        super()
+        this.state ={editing: null}
+        this.handleProductUpdate = this.handleProductUpdate.bind(this)
+    }
 
-export default ProductTable
+    handleProductUpdate(update) {
+        this.setState({editing: null})
+        console.log(update)
+        alert("Product updated")
+    }
+
+    toggleEditing(productId) {
+        this.setState({editing: productId})
+    }
+
+    renderProductOrEditFields(product) {
+        if (this.state.editing === product.id) {
+            return <EditProduct 
+                    key={`editing-${product.id}`}
+                    {...product} 
+                    handleProductUpdate={this.handleProductUpdate}
+                    />
+        } else {
+            return <ProductRow 
+            onClick={this.toggleEditing.bind(this, product.id)} 
+            key={product.id} 
+            {...product}
+            />
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Table striped bordered condensed hover> 
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Description</th>
+                            <th>Brand</th>
+                            <th>Producer</th>
+                            <th>Image URL</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.products.map((product) => {
+                        return this.renderProductOrEditFields(product);
+                    })}
+                    </tbody>
+                </Table> 
+            </div>
+        );
+    }
+}
