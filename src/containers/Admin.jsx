4bbfca55ca2426d10c224250b8e10a8e634f91bf
Linh Do <33480996+grass-jelly@users.noreply.github.com>
2017-11-24 19:47:43 +0700

    modified Admin to show ProductTable

--- src/containers/Admin.jsx
+++ src/containers/Admin.jsx
@@ -3,13 +3,27 @@ import { Provider } from 'react-redux'
 import { createStore} from 'redux'
 import productApp from '../admin-site/reducers'
 import App from '../admin-site/components/App'
+import ProductTable from '../admin-site/components/ProductTable'
 
 let store = createStore(productApp)
+const PRODUCTS = [
+    {id: 1, name: 'p1', price: '$29.99', description: 'a product', 
+    brand: 'Originals', producer: 'adidas', imageUrl: '#'},
+    {id: 2, name: 'p2', price: '$39.99', description: 'a product', 
+    brand: 'NEO', producer: 'adidas', imageUrl: '#'},
+];
+
 const Admin = () => (
     <div style={{position: 'relative', top: 59 + 'px', minHeight: 100 + '%'}}>
-        <Provider store={store}>
-            <App />
-        </Provider> 
+    <ProductTable products={PRODUCTS} />
     </div>
+    
 )
+// const Admin = () => (
+//     <div style={{position: 'relative', top: 59 + 'px', minHeight: 100 + '%'}}>
+//         <Provider store={store}>
+//             <App />
+//         </Provider> 
+//     </div>
+// )
 export default Admin
