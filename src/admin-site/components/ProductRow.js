b3a5f7fdc652534b9a583c6e374f8ddd8ea8f23e
Linh Do <33480996+grass-jelly@users.noreply.github.com>
2017-11-24 19:45:00 +0700

    converted ProductRow to class

--- src/admin-site/components/ProductRow.js
+++ src/admin-site/components/ProductRow.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import { Table, Button, ButtonToolbar } from 'react-bootstrap' 
 
-const ProductRow = ({id, name, price, description, brand, producer, imageUrl}) => (
-    <tr>
-        <td>{id}</td>
-        <td>{name}</td>
-        <td>{price}</td>
-        <td>{description}</td>
-        <td>{brand}</td>
-        <td>{producer}</td>
-        <td>{imageUrl}</td>
-        <td>
-        {
-         <ButtonToolbar>
-             <Button>EDIT</Button>
-             <Button bsStyle="danger">DELETE</Button>
-         </ButtonToolbar>
+export default class ProductRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.id}</td>
+                <td>{this.props.name}</td>
+                <td>{this.props.price}</td>
+                <td>{this.props.description}</td>
+                <td>{this.props.brand}</td>
+                <td>{this.props.producer}</td>
+                <td>{this.props.imageUrl}</td>
+                <td>
+                {
+                <ButtonToolbar>
+                    <Button onClick={this.props.onClick}>EDIT</Button>
+                    <Button bsStyle="danger">DELETE</Button>
+                </ButtonToolbar>
 
-        }
-        </td>
-    </tr>
-)
-
-export default ProductRow
-
+                }
+                </td>
+            </tr>
+        )
+    }
+}
