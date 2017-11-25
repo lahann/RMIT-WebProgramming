541380a3a2c8be0811e3c39382dea4cf67254308
Linh Do <33480996+grass-jelly@users.noreply.github.com>
2017-11-25 17:33:01 +0700

    added AddProduct

---
+++ src/admin-site/components/AddProduct.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import {FormControl, Button, ButtonToolbar} from 'react-bootstrap'
+
+export default class AddProduct extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleAddProduct = this.handleAddProduct.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {
+            id: '', 
+            name: '', 
+            price: '', 
+            description: '', 
+            brand: '', 
+            producer: '', 
+            imageUrl: ''
+        }
+    }
+
+    handleAddProduct() {
+        this.props.handleAddProduct(this.state)
+        this.setState({
+            id: '', 
+            name: '', 
+            price: '', 
+            description: '', 
+            brand: '', 
+            producer: '', 
+            imageUrl: ''
+        })
+    }
+
+    handleChange(e) {
+        let change = {}
+        change[e.target.name] = e.target.value
+        this.setState(change)
+    }
+
+    render() {
+
+        return (
+            <tr>
+            {/* <td>{this.props.id}</td> */}
+            <td>
+                <FormControl
+                    type="text"
+                    value={this.state.id}
+                    name="id"
+                    onChange={this.handleChange}
+                />
+            </td>
+            <td>
+                <FormControl
+                    type="text"
+                    value={this.state.name}
+                    name="name"
+                    onChange={this.handleChange}
+                />
+            </td>
+            <td>
+                <FormControl
+                    type="text"
+                    value={this.state.price}
+                    onChange={this.handleChange}
+                    name="price"
+                />
+            </td>
+            <td>
+                <FormControl
+                    type="text"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                    name="description"
+                />
+            </td>
+            <td>
+                <FormControl
+                    type="text"
+                    value={this.state.brand}
+                    onChange={this.handleChange}
+                    name="brand"
+                />
+            </td>       
+            <td>
+                <FormControl
+                    type="text"
+                    value={this.state.producer}
+                    onChange={this.handleChange}
+                    name="producer"
+                />
+            </td>
+            <td>
+                <FormControl
+                    type="text"
+                    value={this.state.imageUrl}
+                    onChange={this.handleChange}
+                    name="imageUrl"
+                />
+            </td>                          
+            <td>
+            {
+            <ButtonToolbar>
+                <Button bsStyle="success" onClick={this.handleAddProduct}>ADD</Button>
+                <Button bsStyle="danger">DELETE</Button>
+            </ButtonToolbar>
+
+            }
+            </td>
+        </tr>
+
+            
+        )
+    }
+}
